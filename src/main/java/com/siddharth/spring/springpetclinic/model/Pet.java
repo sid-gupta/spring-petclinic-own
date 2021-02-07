28407218c031435a8912847ed458e2223d712cab
sid-gupta <check.sid@gmail.com>
2021-02-07 18:10:23 +0530

    adding POJOs. Closes #2.

---
+++ src/main/java/com/siddharth/spring/springpetclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package com.siddharth.spring.springpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDate;
+    }
+
+    public void setBirthDay(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
---
+++ src/main/java/com/siddharth/spring/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.siddharth.spring.springpetclinic.model;
+
+public class Vet extends Person {
+}

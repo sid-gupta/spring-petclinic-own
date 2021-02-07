252b91034aac703cf8889a9e82ded794422b65d1
sid-gupta <check.sid@gmail.com>
2021-02-08 02:07:57 +0530

    Owners index page and controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/com/siddharth/spring/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package com.siddharth.spring.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @GetMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+
+}
